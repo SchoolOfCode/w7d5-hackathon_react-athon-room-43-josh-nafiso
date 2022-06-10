364baae14577ab3563cc83d2a31821efc7e6466d
nafisoaden97 <103136799+Udecunup5@users.noreply.github.com>
2022-06-10 11:39:18 +0100

    created add button

--- todo-list-app/src/components/app/App.js
+++ todo-list-app/src/components/app/App.js
@@ -1,28 +1,45 @@
 import "./App.css";
 import React from "react";
-import { useState } from "react";
+import {useState} from "react";
 import Input from "../input";
 
 function App() {
-  const [text, setText] = useState("");
+    const [text, setText] = useState("");
 
-  function handleChange(userInput) {
-    setText(userInput);
-    console.log(text);
-  }
+    function handleChange(userInput) {
+        setText(userInput);
+        // console.log(text);
+    }
+    const [list, setList] = useState([])
+    function HandleClick() {
+        const newList = text
+        console.log(newList);
+        setText("")
+        setList(newList)
 
-  return (
-    <main className="App">
-      <header className="App-header">
-        <h1>ToDo List</h1>
-        <Input
-          text={text}
-          handleChange={handleChange}
-          placeHolder="Write a new task..."
-        />
-      </header>
-    </main>
-  );
+    }
+    // let getValue = (i, e) => {
+    //   let newInput = [...input];
+    //   newInput[i][e.target.name] = e.target.value;
+    //   setInput(newInput);
+    // }
+
+    return (
+        <main className="App">
+            <header className="App-header">
+                <h1>ToDo List</h1>
+                <Input text={text}
+                    handleChange={handleChange}
+                    placeHolder="Write a new task..."/>
+                <button onClick={HandleClick}>
+                    add to list
+                </button>
+             
+                    <li>{list}</li>
+            
+            </header>
+        </main>
+    );
 }
 
 export default App;
